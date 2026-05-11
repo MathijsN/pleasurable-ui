f2b575de7725c9f32f3920ac21aefc0c2614782f
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-05-11 11:50:31 +0200

    file setup

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import { Liquid } from 'liquidjs';
 const app = express()
 
 // Maak werken met data uit formulieren iets prettiger
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 // Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
 // Bestanden in deze map kunnen dus door de browser gebruikt worden
@@ -25,6 +25,9 @@ app.set('views', './views')
 
 
 app.get('/', async function (request, response) {
+
+
+
   response.render('index.liquid')
 })
 
