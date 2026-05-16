6a02d598b65f768b27ea3e2da1d5f81cb081ff7b
MathijsN <72303334+MathijsN@users.noreply.github.com>
2026-05-16 11:34:28 +0200

    feat: client-side JS for loading #26

--- public/script.js
+++ public/script.js
@@ -8,3 +8,45 @@ document.getElementById('file').onchange = function (evt) {
         preview.src = URL.createObjectURL(file);
     }
 }
+
+const pictureForm = document.querySelector("form.picture")
+const snapps = document.getElementById("grid")
+
+pictureForm.addEventListener('submit', async function (e) {
+    e.preventDefault()
+
+    document.querySelector(".loader").style.display = 'block'
+    document.querySelector(".submit-text").style.display = 'none'
+
+    let formData = new FormData(pictureForm);
+
+    const response = await fetch(pictureForm.action, {
+        method: 'POST',
+        body: formData // <<< Dit moet omdat server.js anders niet met de formulier data kan werken
+    })
+
+    if (response.ok) {
+
+        const responseData = await response.text()
+
+        const parser = new DOMParser()
+        const responseDOM = parser.parseFromString(responseData, 'text/html')
+
+        // Zoek in die nieuwe HTML DOM onze nieuwe UI state op, die we via Liquid hebben klaargemaakt
+        const newState = responseDOM.querySelector('ul#grid')
+
+        snapps.innerHTML = newState.innerHTML
+
+        document.getElementById('file').value = ''
+        document.getElementById('succes-dialog').show()
+    } else {
+        document.getElementById('failure-dialog').show()
+    }
+
+
+
+    // Loading state weghalen
+    // Nu kan je waarschijnlijk de Loading state vervangen door een Success state
+    document.querySelector(".loader").style.display = 'none'
+    document.querySelector(".submit-text").style.display = 'block'
+})
